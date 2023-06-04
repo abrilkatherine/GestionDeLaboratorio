d6ff25e934796196071665b73d92ec0c83e16d59
abrilkatherine <abrilkatherinec@gmail.com>
2023-06-04 00:01:54 -0300

    feat: main test

--- src/main/uade/edu/ar/Main.java
+++ src/main/uade/edu/ar/Main.java
@@ -1,11 +1,23 @@
 package main.uade.edu.ar;
 
-import main.uade.edu.ar.controller.Controller;
+import main.uade.edu.ar.controller.PacienteController;
+import main.uade.edu.ar.dto.PacienteDto;
+import main.uade.edu.ar.model.Genero;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        Controller controller = Controller.getInstance();
+        try {
+            PacienteController pacienteController = PacienteController.getInstance();
+
+            pacienteController.crearPaciente(new PacienteDto(1, "Calabaza", 123, "", "", "pepe", 20, Genero.MASCULINO));
+            pacienteController.crearPaciente(new PacienteDto(2, "Calabaza", 123, "", "", "pepe", 20, Genero.MASCULINO));
+            pacienteController.borrarPaciente(1);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
